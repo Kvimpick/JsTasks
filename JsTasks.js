42d4d53ec1cfbcffa07b16673ee390c34d5995cb
Kvimpick <galaydataras@gmail.com>
2020-12-14 12:06:27 +0200

    Some other task

--- JsTasks.js
+++ JsTasks.js
@@ -182,3 +182,56 @@
 // }
 // addAffair(2019, 02, 25, 'дело411')
 // console.log(affairs)
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Дан такой массив с юзерами:
+//
+// Сделайте две функции, которые будут менять значение поля active: первая функция пусть меняет на true, а вторая - на false.
+//
+// Пусть наши функции параметрами принимают данный массив и номер юзера, над которым следует произвести изменения.
+//
+// Пусть функции возвращают измененный массив.
+
+// let users = [
+// 	{
+// 		name: 'name1',
+// 		active: true,
+// 	},
+// 	{
+// 		name: 'name2',
+// 		active: false,
+// 	},
+// 	{
+// 		name: 'name3',
+// 		active: true,
+// 	},
+// 	{
+// 		name: 'name4',
+// 		active: false,
+// 	},
+// 	{
+// 		name: 'name5',
+// 		active: true,
+// 	},
+// ];
+//
+// function activeTrue(array, usersName) {
+//     for (let item of users){
+//     if (item.name === usersName){
+//         item.active = true
+//         }
+//     }
+//     return users
+// }
+//
+// function activeFalse(array, usersName) {
+//     for (let item of users){
+//         if (item.name === usersName){
+//             item.active = false
+//         }
+//     }
+//     return users
+// }
+//
+// console.log(activeFalse(users, 'name1'))
